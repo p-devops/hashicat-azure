43535accdcd2383638b2960354b52b24340f2dad
Peter Leung <pleung@hashicorp.com>
2021-07-15 12:33:52 -0400

    Update logo

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,8 +9,8 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/da/Willis_Towers_Watson_logo.svg/1024px-Willis_Towers_Watson_logo.svg.png"></img></center>
-  <center><h2>Welcome to Willis Towers Watson</h2></center>
+  <center><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Qurate_Retail_logo.svg/1200px-Qurate_Retail_logo.svg.png"></img></center>
+  <center><h2>Welcome to Qurate Retail!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own. 
   <!-- END -->
   
